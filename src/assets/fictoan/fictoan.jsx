bede22f54a6213ed193899801a7c0ac8cf8cbebd
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2019-04-24 13:00:29 +0530

    Removed superfluous nested component folders

--- src/assets/fictoan/fictoan.jsx
+++ src/assets/fictoan/fictoan.jsx
@@ -6,17 +6,17 @@ import Card from './components/Card/Card'
 import Header from './components/Header/Header'
 
 import PageWithSidebar from './components/PageWithSidebar/PageWithSidebar'
-import SidebarItemIcon from './components/PageWithSidebar/Components/SidebarWrapper/Components/SidebarItemIcon/SidebarItemIcon'
-import SidebarItemText from './components/PageWithSidebar/Components/SidebarWrapper/Components/SidebarItemText/SidebarItemText'
-import SidebarItem from './components/PageWithSidebar/Components/SidebarWrapper/Components/SidebarItem/SidebarItem'
-import SidebarWrapper from './components/PageWithSidebar/Components/SidebarWrapper/SidebarWrapper'
-import ContentWrapper from './components/PageWithSidebar/Components/ContentWrapper/ContentWrapper'
+import SidebarItemIcon from './components/PageWithSidebar/SidebarWrapper/SidebarItemIcon/SidebarItemIcon'
+import SidebarItemText from './components/PageWithSidebar/SidebarWrapper/SidebarItemText/SidebarItemText'
+import SidebarItem from './components/PageWithSidebar/SidebarWrapper/SidebarItem/SidebarItem'
+import SidebarWrapper from './components/PageWithSidebar/SidebarWrapper/SidebarWrapper'
+import ContentWrapper from './components/PageWithSidebar/ContentWrapper/ContentWrapper'
 
 import Form from "./components/Form/Form";
-import FormUnit from "./components/Form/Components/FormUnit/FormUnit";
-import FormGroup from "./components/Form/Components/FormGroup/FormGroup";
-import InputField from "./components/Form/Components/InputField/InputField";
-import InputLabel from "./components/Form/Components/InputLabel/InputLabel";
+import FormUnit from "./components/Form/FormUnit/FormUnit";
+import FormGroup from "./components/Form/FormGroup/FormGroup";
+import InputField from "./components/Form/InputField/InputField";
+import InputLabel from "./components/Form/InputLabel/InputLabel";
 
 export {
     Row,
