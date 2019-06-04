5bb3c4046069877a196ad508e8b9be4b8de60763
Gandharva <gandharva@Gandharvas-MacBook-Pro.local>
2019-06-04 12:26:09 +0530

    Radio button on click event including label click support.

--- src/assets/fictoan/components/Form/RadioGroup/RadioButton.jsx
+++ src/assets/fictoan/components/Form/RadioGroup/RadioButton.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 function RadioButton(props) {
     return (
-        <div className="ff-radio">
+        <div className="ff-radio" onClick={()=>typeof props.onClick === 'function' && props.onClick(props.id, !props.isChecked)}>
             <input
                 type="radio"
                 id={props.id}
-                name={props.name} />
-
+                name={props.name}
+                checked={props.isChecked} />    
             <label htmlFor={props.id}>{props.value}</label>
         </div>
     )
