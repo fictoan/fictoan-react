caf333ff348b6a03bf252ce9f8ffd3c68e2ae766
Rohit Singhal <rohit@setu.co>
2019-07-01 10:49:59 +0530

    InputField update

--- src/components/Form/InputField/InputField.jsx
+++ src/components/Form/InputField/InputField.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
 function InputField(props) {
-    return (
-        <input
-            id={props.id}
-            className={`ff-input ${props.className || ""}`}
-            placeholder=" "
-            value={props.value}
-            type={props.type}
-            required={props.required}
-            pattern={props.pattern}
-            readOnly={props.readonly}
-        />
-    )
+	return (
+		<input
+			id={props.id}
+			className={`ff-input ${props.className || ""}`}
+            placeholder={props.placeholder}
+			value={props.value}
+			type={props.type}
+			required={props.required}
+			pattern={props.pattern}
+			readOnly={props.readonly}
+			onChange={props.onChange}
+		/>
+	)
 }
 
 export default InputField
