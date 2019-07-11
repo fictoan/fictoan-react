4a7aeb3f9ae8e3afa817fa7a718dc4f99bfac310
Rohit Singhal <rohit@setu.co>
2019-07-11 11:04:33 +0530

    Table component and styling

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,21 @@
-import Row from './components/Row/Row'
-import Portion from './components/Portion/Portion'
+import Row from "./components/Row/Row";
+import Portion from "./components/Portion/Portion";
 
-import Card from './components/Card/Card'
+import Card from "./components/Card/Card";
 
-import Header from './components/Header/Header'
+import Header from "./components/Header/Header";
 
-import SidebarItemIcon from './components/SidebarWrapper/SidebarItemIcon/SidebarItemIcon'
-import SidebarItemText from './components/SidebarWrapper/SidebarItemText/SidebarItemText'
-import SidebarItem from './components/SidebarWrapper/SidebarItem/SidebarItem'
-import SidebarHeader from './components/SidebarWrapper/SidebarHeader/SidebarHeader'
-import SidebarWrapper from './components/SidebarWrapper/SidebarWrapper'
+import SidebarItemIcon from "./components/SidebarWrapper/SidebarItemIcon/SidebarItemIcon";
+import SidebarItemText from "./components/SidebarWrapper/SidebarItemText/SidebarItemText";
+import SidebarItem from "./components/SidebarWrapper/SidebarItem/SidebarItem";
+import SidebarHeader from "./components/SidebarWrapper/SidebarHeader/SidebarHeader";
+import SidebarWrapper from "./components/SidebarWrapper/SidebarWrapper";
 
-import ContentWrapper from './components/ContentWrapper/ContentWrapper'
+import ContentWrapper from "./components/ContentWrapper/ContentWrapper";
 
-import HRule from "./components/HRule/HRule"
+import HRule from "./components/HRule/HRule";
 
-import Button from "./components/Button/Button"
+import Button from "./components/Button/Button";
 
 import Form from "./components/Form/Form";
 import FormUnit from "./components/Form/FormUnit/FormUnit";
@@ -24,6 +24,8 @@ import InputField from "./components/Form/InputField/InputField";
 import TextArea from "./components/Form/TextArea/TextArea";
 import InputLabel from "./components/Form/InputLabel/InputLabel";
 import FileUpload from "./components/Form/FileUpload/FileUpload";
+
+import Table from "./components/Table/Table";
 
 export {
     Row,
@@ -44,5 +46,6 @@ export {
     InputLabel,
     Button,
     FileUpload,
-    HRule
-}
+    HRule,
+    Table
+};
