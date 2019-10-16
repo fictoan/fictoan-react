71f954f0a8b227037b51e9b4a89536a8a78ab7a3
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2019-10-16 09:14:22 +0530

    Add gutter props for Row

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -7,15 +7,22 @@ import { BaseAndHTMLProps } from "../BaseComponent/typings";
 
 interface RowProps extends BaseAndHTMLProps<HTMLDivElement> {
     contentPadding?: "tiny" | "small" | "medium" | "large" | "huge";
+    gutters?: "tiny" | "small" | "medium" | "large" | "huge";
 }
 
-const Row = ({contentPadding, className, ...props}: RowProps) => {
+const Row = ({contentPadding, className, gutters, ...props}: RowProps) => {
     const classNames = [
         className
     ]
 
     if (contentPadding) {
         classNames.push(`content-padding-${contentPadding}`)
+    }
+
+    if (gutters) {
+        classNames.push(`${contentPadding}-gutters`)
+    } else {
+        classNames.push("no-gutters")
     }
 
     return (
