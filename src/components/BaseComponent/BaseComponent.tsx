1274b56ec5a0837e98e77051e78ce56eec447aea
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2019-10-18 14:58:19 +0530

    add show/hide props

--- src/components/BaseComponent/BaseComponent.tsx
+++ src/components/BaseComponent/BaseComponent.tsx
@@ -17,14 +17,31 @@ export const BaseComponent = <K extends {}>({
     className,
     bgColor,
     textColor,
+    hideOnMobile,
+    showOnlyOnMobile,
+    hideOnTabPT,
+    showOnlyOnTabPT,
+    hideOnTabLS,
+    showOnlyOnTabLS,
+    hideOnDesktop,
+    showOnlyOnDesktop,
     ...props
 }: HTMLProps<K> & BaseComponentProps) => (
     <Element {...props} className={
         createClassName([
             baseClassName,
             className,
+            hideOnMobile,
             bgColor && `bg-${bgColor}`,
-            textColor && `text-${textColor}`
+            textColor && `text-${textColor}`,
+            hideOnMobile && "hide-on-mobile",
+            showOnlyOnMobile && "show-only-on-mobile",
+            showOnlyOnTabPT && "hide-on-tab-pt",
+            showOnlyOnTabPT && "show-only-on-tab-pt",
+            showOnlyOnTabLS && "hide-on-tab-ls",
+            showOnlyOnTabLS && "show-only-on-tab-ls",
+            hideOnDesktop && "hide-on-desktop",
+            showOnlyOnDesktop && "show-only-on-desktop",
             ]
         )}
     />
