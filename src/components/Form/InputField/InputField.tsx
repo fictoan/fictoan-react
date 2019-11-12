7888ed25c0f36f85e280660c619e84f50e3d152c
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2019-11-12 10:33:19 +0530

    Changs to Input Wrapper

--- src/components/Form/InputField/InputField.tsx
+++ src/components/Form/InputField/InputField.tsx
@@ -2,15 +2,35 @@ import React, { PureComponent } from "react";
 
 import { BaseComponent } from "../../BaseComponent/BaseComponent";
 import { InputFieldProps } from "./typings";
+import { createClassName } from "../../../utils/classNames";
 
 class InputField extends PureComponent<InputFieldProps> {
     render() {
-        const { iconLeft, iconRight, ...inputProps } = this.props;
+        const {
+            className,
+            iconLeft,
+            iconRight,
+            ...inputProps
+        } = this.props;
+
+        const classNames = [
+            className
+        ]
+
+        if (iconLeft) {
+            classNames.push("with-icon-left")
+        }
+
+        if (iconRight) {
+            classNames.push("with-icon-right")
+        }
+
         return (
             <>
                 <BaseComponent<HTMLInputElement>
                     Element="input"
                     baseClassName="ff-input"
+                    className={createClassName(classNames)}
                     {...inputProps}
                 />
                 {iconLeft && <span className="icon-left">{iconLeft}</span>}
