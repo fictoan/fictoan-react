75f83d80b7cebe303bb223822fbe3f1aa78df476
Rohit Singhal <rohit@setu.co>
2019-12-23 15:04:40 +0530

    language codeblock

--- src/components/CodeBlock/constants.ts
+++ src/components/CodeBlock/constants.ts
@@ -4,6 +4,7 @@ export type  CodeBlockElementType = HTMLPreElement;
 
 export interface CodeBlockCustomProps {
     source: object | string;
+    language?: string;
 }
 
 export type CodeBlockProps = BaseAndHTMLProps<CodeBlockElementType> & CodeBlockCustomProps;
