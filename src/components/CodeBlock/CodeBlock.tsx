438be838b940b8f9c3af54c5e96fc117c8231a0c
Rohit Singhal <rohit@setu.co>
2019-12-23 15:13:05 +0530

    codeblocks update

--- src/components/CodeBlock/CodeBlock.tsx
+++ src/components/CodeBlock/CodeBlock.tsx
@@ -20,23 +20,15 @@ export const CodeBlock = ({ ref, source, theme, ...props }: CodeBlockProps) => {
         >
             {({ className, style, tokens, getLineProps, getTokenProps }) => (
                 <pre className={className} style={style}>
-                    {tokens.map((line, i) => (
-                        <div
-                            {...getLineProps({
-                                line,
-                                key: i
-                            })}
-                        >
-                            {line.map((token, key) => (
-                                <span
-                                    {...getTokenProps({
-                                        token,
-                                        key
-                                    })}
-                                />
-                            ))}
-                        </div>
-                    ))}
+                    <code>
+                        {tokens.map((line, i) => (
+                            <div {...getLineProps({ line, key: i })}>
+                                {line.map((token, key) => (
+                                    <span {...getTokenProps({ token, key })} />
+                                ))}
+                            </div>
+                        ))}
+                    </code>
                 </pre>
             )}
         </PrismReactRenderer>
--- src/components/CodeBlock/constants.ts
+++ src/components/CodeBlock/constants.ts
@@ -1,10 +1,12 @@
+import { Language } from "prism-react-renderer";
+
 import { BaseAndHTMLProps } from "../BaseComponent/typings";
 
 export type  CodeBlockElementType = HTMLPreElement;
 
 export interface CodeBlockCustomProps {
     source: object | string;
-    language?: string;
+    language?: Language;
 }
 
 export type CodeBlockProps = BaseAndHTMLProps<CodeBlockElementType> & CodeBlockCustomProps;
