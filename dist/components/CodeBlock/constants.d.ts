84a81d8305d1e73ccb8697d75163f772af69717a
Rohit Singhal <rohit@setu.co>
2019-12-23 15:06:16 +0530

    Updates

--- dist/components/CodeBlock/constants.d.ts
+++ dist/components/CodeBlock/constants.d.ts
@@ -2,5 +2,6 @@ import { BaseAndHTMLProps } from "../BaseComponent/typings";
 export declare type CodeBlockElementType = HTMLPreElement;
 export interface CodeBlockCustomProps {
     source: object | string;
+    language?: string;
 }
 export declare type CodeBlockProps = BaseAndHTMLProps<CodeBlockElementType> & CodeBlockCustomProps;
