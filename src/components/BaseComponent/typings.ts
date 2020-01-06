2ec001353b3439aa9f7adbf813bdb71cc97a4680
sujan <sujan.sundareswaran@gmail.com>
2020-01-06 20:03:26 +0530

    Fix indent

--- src/components/BaseComponent/typings.ts
+++ src/components/BaseComponent/typings.ts
@@ -1,32 +1,30 @@
 import { HTMLProps } from "react";
-import { RFTheme } from "src/constants/Theme";
+import { RFTheme }   from "src/constants/Theme";
 
 export interface BaseProps {
-    theme?             : typeof RFTheme;
-
-    // size?: string;
-    bgColor?           : string;
-    textColor?         : string;
-    hideOnMobile?      : boolean;
-    showOnlyOnMobile?  : boolean;
-    hideOnTabPT?       : boolean;
-    showOnlyOnTabPT?   : boolean;
-    hideOnTabLS?       : boolean;
-    showOnlyOnTabLS?   : boolean;
-    hideOnDesktop?     : boolean;
-    showOnlyOnDesktop? : boolean;
-    fullWidth?         : boolean;
-    fullHeight?        : boolean;
-    marginTop?         : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    marginRight?       : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    marginBottom?      : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    marginLeft?        : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    margin?            : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    paddingTop?        : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    paddingRight?      : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    paddingBottom?     : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    paddingLeft?       : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
-    padding?           : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+theme             ? : typeof RFTheme;
+bgColor           ? : string;
+textColor         ? : string;
+hideOnMobile      ? : boolean;
+showOnlyOnMobile  ? : boolean;
+hideOnTabPT       ? : boolean;
+showOnlyOnTabPT   ? : boolean;
+hideOnTabLS       ? : boolean;
+showOnlyOnTabLS   ? : boolean;
+hideOnDesktop     ? : boolean;
+showOnlyOnDesktop ? : boolean;
+fullWidth         ? : boolean;
+fullHeight        ? : boolean;
+marginTop         ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+marginRight       ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+marginBottom      ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+marginLeft        ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+margin            ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+paddingTop        ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+paddingRight      ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+paddingBottom     ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+paddingLeft       ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
+padding           ? : "none" | "fixed" | "tiny" | "small" | "medium" | "large" | "huge";
 }
 
 export interface BaseAndHTMLProps<T extends {}> extends BaseProps, Omit<HTMLProps<T>, "size"> { }
