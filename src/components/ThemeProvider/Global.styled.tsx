efa2b2ba80b25e41473aeb8f2d1c5c6a8805e1ef
sujan <sujan.sundareswaran@gmail.com>
2020-01-07 13:57:33 +0530

    Add theme variables global typography rules

--- src/components/ThemeProvider/Global.styled.tsx
+++ src/components/ThemeProvider/Global.styled.tsx
@@ -1,24 +1,25 @@
 import { createGlobalStyle } from "styled-components";
+
 import { GlobalStyledProps } from "./constants";
 
 export const GlobalStyled = createGlobalStyle`
     body {
         font-family      : ${(props: GlobalStyledProps) => props.theme};
         font-size        : ${(props: GlobalStyledProps) => props.theme.body};
-        line-height      : $bodyLineHeight;
-        color            : $bodyFontColor;
+        line-height      : ${(props: GlobalStyledProps) => props.theme.text.paras.lineHeight};
+        color            : ${(props: GlobalStyledProps) => props.theme.text.paras.color};
         word-wrap        : normal;
         text-rendering   : optimizeLegibility;
-        background-color : $bodyBgColor;
+        background-color : ${(props: GlobalStyledProps) => props.theme.body.bg};
     }
 
     h1, h2, h3, h4, h5, h6 {
-        font-weight : $headingFontWeight;
+        font-weight : ${(props: GlobalStyledProps) => props.theme.text.headings.weight};
         margin      : 0;
     }
 
     h1 { line-height : 12; }
-    h2, h3, h4, h5, h6 { line-height : $headingLineHeight; }
+    h2, h3, h4, h5, h6 { line-height : ${(props: GlobalStyledProps) => props.theme.text.headings.lineHeight}; }
 
     h1 { font-size : ${(props: GlobalStyledProps) => props.theme.text.size.default * Math.pow(props.theme.text.size.multiplier, 8) }; }
     h2 { font-size : ${(props: GlobalStyledProps) => props.theme.text.size.default * Math.pow(props.theme.text.size.multiplier, 6) }; }
@@ -41,12 +42,12 @@ export const GlobalStyled = createGlobalStyle`
 
     a {
         cursor      : pointer;
-        color       : $linkColor;
+        color       : ${(props: GlobalStyledProps) => props.theme.text.links.default};
         font-weight : bold;
         transition  : all 0.2s;
     }
 
-    a:not(.ff-button):not(.ff-pill):not(.header-links a):hover { color : $linkHoverColor; }
+    a:not(.ff-button):not(.ff-pill):not(.header-links a):hover { color : ${(props: GlobalStyledProps) => props.theme.text.links.onHover}; }
 
     a:active { opacity: 0.72; }
 `
--- src/components/Typography/Heading.tsx
+++ src/components/Typography/Heading.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { createClassName } from "src/utils/classNames";
 import { BaseComponent } from "../BaseComponent/BaseComponent";
 
-import { HeadingStyled } from "./TypographyStyled";
 import { HeadingProps, HeadingElementType } from "./constants";
 
 export const Heading = ({ as, weight, className, ...props }: HeadingProps) => {
