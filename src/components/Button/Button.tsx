17472259c6358c4f041c35d5e6b916fb5a01a5f2
sujan <sujan.sundareswaran@gmail.com>
2020-02-06 12:49:54 +0530

    Tweak button component to Primary and Secondary styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,27 +6,31 @@ import { Element } from "../Element/Element";
 import { ButtonStyled } from "./Button.styled"
 import { ButtonProps, ButtonElementType } from "./constants";
 
-export const Button = ({ size, shape, shadow, border, className, isLoading, hasDelete, ...props }: ButtonProps) => {
+export const Button = ({ size, shape, shadow, border, type, isLoading, hasDelete, className, ...props }: ButtonProps) => {
     const classNames = [className];
 
     if (className) {
         classNames.push(className);
     }
 
+    if (type) {
+        classNames.push(type);
+    }
+
     if (size) {
-        classNames.push(`size-${size}`)
+        classNames.push(`size-${size}`);
     }
 
     if (shape) {
-        classNames.push(`shape-${shape}`)
+        classNames.push(`shape-${shape}`);
     }
 
     if (shadow) {
-        classNames.push(`shadow-${shadow}`)
+        classNames.push(`shadow-${shadow}`);
     }
 
     if (border) {
-        classNames.push(`border-${border}`)
+        classNames.push(`border-${border}`);
     }
 
     if (isLoading) {
