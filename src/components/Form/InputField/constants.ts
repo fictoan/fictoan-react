2595555ba3131566550f9481387fa8c37f6ef566
sujan <sujan.sundareswaran@gmail.com>
2020-03-03 11:37:10 +0530

    Fix input custom props bug

--- src/components/Form/InputField/constants.ts
+++ src/components/Form/InputField/constants.ts
@@ -1,8 +1,8 @@
 import { CommonAndHTMLProps } from "../../Element/constants";
 
-import { InputLabelProps } from "../InputLabel/constants";
+import { InputLabelCustomProps } from "../InputLabel/constants";
 import { InputCommonProps, InputIconProps } from "../BaseInputComponent/constants";
 
 export type InputFieldElementType = HTMLInputElement;
 
-export type InputFieldProps = CommonAndHTMLProps<InputFieldElementType> & InputLabelProps & InputCommonProps & InputIconProps;
+export type InputFieldProps = CommonAndHTMLProps<InputFieldElementType> & InputLabelCustomProps & InputCommonProps & InputIconProps;
