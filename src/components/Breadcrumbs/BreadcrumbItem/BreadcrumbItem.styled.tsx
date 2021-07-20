00eca74a0c23799017b80a7de559423a74d0dde9
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2021-07-20 19:29:02 +0530

    Rename BreadcrumbWrapper to plural BreadcrumbsWrapper and add better styling

--- src/components/Breadcrumbs/BreadcrumbItem/BreadcrumbItem.styled.tsx
+++ src/components/Breadcrumbs/BreadcrumbItem/BreadcrumbItem.styled.tsx
@@ -4,22 +4,28 @@ import { BreadcrumbItemProps } from "./BreadcrumbItem";
 
 
 export const BreadcrumbItemStyled = styled.li`
-    display      : inline-flex;
-    position     : relative;
-    font-size    : 14px;
+    display   : inline-flex;
+    position  : relative;
+    font-size : 14px;
 
     &::after {
         position : absolute;
-        content  : "/";
         right    : -14px;
-        color    : ${(props : BreadcrumbItemProps) => props.theme.breadcrumbs.item.separator};
+        content  : ${(props : BreadcrumbItemProps) => props.theme.breadcrumbs.separator.content};
+        color    : ${(props : BreadcrumbItemProps) => props.theme.breadcrumbs.separator.text};
     }
 
     &:last-child,
-    &:last-child a { color : ${(props: BreadcrumbItemProps) => props.theme.breadcrumbs.item.active}; }
+    &:last-child a {
+        color : ${(props : BreadcrumbItemProps) => props.theme.breadcrumbs.item.active};
+    }
 
     &:not(:last-child),
-    &:not(:last-child) a { color : ${(props: BreadcrumbItemProps) => props.theme.breadcrumbs.item.inactive}; }
+    &:not(:last-child) a {
+        color : ${(props : BreadcrumbItemProps) => props.theme.breadcrumbs.item.inactive};
+    }
 
-    &:last-child::after { content : ""; }
+    &:last-child::after {
+        content : "";
+    }
 `;
