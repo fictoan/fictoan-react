306d492ea6aa8f837126954a8129b2c7a3f87d98
Naresh <ghostwriternr@gmail.com>
2022-05-28 18:08:53 +0530

    Fix BottomDrawer overlay default theme not picked up

--- src/components/BottomDrawer/BottomDrawer.styled.tsx
+++ src/components/BottomDrawer/BottomDrawer.styled.tsx
@@ -15,8 +15,8 @@ export const BottomDrawerStyled = styled.div`
         height           : 100vh;
         backdrop-filter   : blur(20px);
         background-color : ${(props: BottomDrawerProps) =>
-                            `${convertToFictoanColor(
-                                props.overlayColour ?? (props.theme.bottomDrawer.overlay.bg as ColourPropTypes)
+                            `${(
+                                convertToFictoanColor(props.overlayColour) ?? props.theme.bottomDrawer.overlay.bg
                             ).replace(
                                 /(^hsla\(.*,.*,.*,)(.*)(\))$/,
                                 `$1${(
