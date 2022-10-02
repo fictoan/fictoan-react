4e9766e9a5bad06dc2f810378fb877f41b9d6957
Sreejit7 <sreejitde7@gmail.com>
2022-10-02 13:18:26 +0530

    enhancement(tabs): add support for additional items in Tabs navbar

--- src/components/Tabs/Tabs.styled.tsx
+++ src/components/Tabs/Tabs.styled.tsx
@@ -4,53 +4,58 @@ export const TabsStyled = styled.section`
     nav {
         width   : 100%;
         display : flex;
+        align-items: center;
 
-        p { position : relative; }
+        ul.tab-labels-list {
+            display: flex;
 
-        p:not(last-child) {
-            margin-right  : 24px;
-            color         : ${props => props.theme.tabs.label.default.text};
-            font-weight   : 400;
-            padding       : 16px 0;
-            border-bottom : 2px solid transparent;
-        }
-    
-        p:hover { color : ${props => props.theme.tabs.label.onHover.text}; }
+            p { position : relative; }
 
-        p.is-active { color : ${props => props.theme.tabs.label.isActive.text}; }
+            li:not(last-child) > p {
+                margin-right  : 24px;
+                color         : ${props => props.theme.tabs.label.default.text};
+                font-weight   : 400;
+                padding       : 16px 0;
+                border-bottom : 2px solid transparent;
+            }
         
-        p.is-right-aligned {
-            margin-left  : auto;
-            margin-right : 0;
-        }
+            p:hover { color : ${props => props.theme.tabs.label.onHover.text}; }
 
-        p.is-active::after {
-            display                 : block;
-            position                : absolute;
-            width                   : 100%;
-            height                  : 3px;
-            bottom                  : -3px;
-            left                    : 0;
-            content                 : "";
-            border-top-left-radius  : 3px;
-            border-top-right-radius : 3px;
-            background-color        : ${props => props.theme.tabs.label.isActive.border};
-        }
-    
-        p.has-alert::after {
-            position         : absolute;
-            top              : 12px;
-            left             : 94%;
-            border           : 3px solid ${props => props.theme.tabs.label.hasAlert.circle.border};
-            width            : 14px;
-            height           : 14px;
-            background-color : ${props => props.theme.tabs.label.hasAlert.circle.bg};
-            border-radius    : 50%;
-        }
-    
-        p.disabled-navlink {
-            color  : ${props => props.theme.tabs.label.isDisabled.text};;
-            cursor : not-allowed;
+            p.is-active { color : ${props => props.theme.tabs.label.isActive.text}; }
+            
+            p.is-right-aligned {
+                margin-left  : auto;
+                margin-right : 0;
+            }
+
+            p.is-active::after {
+                display                 : block;
+                position                : absolute;
+                width                   : 100%;
+                height                  : 3px;
+                bottom                  : -3px;
+                left                    : 0;
+                content                 : "";
+                border-top-left-radius  : 3px;
+                border-top-right-radius : 3px;
+                background-color        : ${props => props.theme.tabs.label.isActive.border};
+            }
+        
+            p.has-alert::after {
+                position         : absolute;
+                top              : 12px;
+                left             : 94%;
+                border           : 3px solid ${props => props.theme.tabs.label.hasAlert.circle.border};
+                width            : 14px;
+                height           : 14px;
+                background-color : ${props => props.theme.tabs.label.hasAlert.circle.bg};
+                border-radius    : 50%;
+            }
+        
+            p.disabled-navlink {
+                color  : ${props => props.theme.tabs.label.isDisabled.text};;
+                cursor : not-allowed;
+            }
         }
     }
 
