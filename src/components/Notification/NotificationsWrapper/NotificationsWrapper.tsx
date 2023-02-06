261c3dafff4771362fae3035927fba3320c8f9ef
Vivek G <audit536@gmail.com>
2023-02-06 22:32:52 +0530

    Update to proper typescript refs and fix class add and remove logic

--- src/components/Notification/NotificationsWrapper/NotificationsWrapper.tsx
+++ src/components/Notification/NotificationsWrapper/NotificationsWrapper.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createRef } from "react";
 
 import { Element } from "../../Element/Element";
 import { CommonAndHTMLProps } from "../../Element/constants";
@@ -21,13 +21,17 @@ export const NotificationsWrapper = React.forwardRef(
         { position = "right", anchor = "top", order = "new-on-top", ...props }: NotificationsWrapperProps,
         ref: React.Ref<NotificationsWrapperElementType>
     ) => {
-        let classNames = [];
+        let classNames = ["hide"];
 
-        if(ref.current.childElementCount > 0){
-            classNames.push(show)
+        const notificationsWrapperNode = createRef<HTMLDivElement>()
+
+        if(notificationsWrapperNode.current.childElementCount > 0){
+            classNames = classNames.filter(item => item!="hide")
+            classNames.push("show")
         }
         else{
-            classNames.push(hide)
+            classNames = classNames.filter(item => item!="show")
+            classNames.push("hide")
         }
 
         if (position) {
