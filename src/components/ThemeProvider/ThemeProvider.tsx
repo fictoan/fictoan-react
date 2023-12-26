8c400556e2da698dca2902a34705cdb2556eccaa
dhruvmisra <dhruvmisra@live.com>
2023-12-26 20:36:17 +0530

    Support customColors with theme provider

--- src/components/ThemeProvider/ThemeProvider.tsx
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -6,36 +6,78 @@ import { CommonAndHTMLProps } from "../Element/constants";
 export type ThemeProviderElementType = HTMLDivElement;
 export type RenderProps = () => JSX.Element;
 
-type Theme = {
-    [key: string]: string;
-};
-
 export interface ThemeProviderProps extends CommonAndHTMLProps<ThemeProviderElementType> {
-    theme?: Theme;
+    theme?: {
+        [key: string]: string;
+    };
+    customColors?: {
+        [key: string]: string;
+    };
     currentTheme?: string;
     onThemeInjection?: () => void;
 }
 
 export const ThemeProvider = React.forwardRef(
     (
-        { theme, currentTheme, onThemeInjection, children }: ThemeProviderProps,
+        { theme, customColors, currentTheme, onThemeInjection, children, ...props }: ThemeProviderProps,
         ref: React.Ref<ThemeProviderElementType>
     ) => {
         useEffect(() => {
             if (theme) {
-                Object.entries(theme).forEach(([variableName, variableValue]) => {
-                    document.documentElement.style.setProperty(`--${variableName}`, variableValue);
-                });
+                const styleTag = getStyleTag("fictoan-theme");
+                addCssVariables(styleTag, theme);
+                onThemeInjection && onThemeInjection();
             }
-            onThemeInjection && onThemeInjection();
 
             return () => {
-                document.documentElement.removeAttribute("style");
+                const styleTag = getStyleTag("fictoan-theme");
+                styleTag.innerHTML = "";
             };
         }, [theme]);
 
+        useEffect(() => {
+            if (customColors) {
+                const styleTag = getStyleTag("fictoan-custom-colors");
+                addCssVariables(styleTag, customColors);
+                const styles = Object.keys(customColors).map(
+                    (colorName) => `
+.bg-${colorName} {background-color: var(--${colorName});}
+.text-${colorName} {color: var(--${colorName}); }
+.fill-${colorName} {fill: var(--${colorName});}
+.stroke-${colorName} {stroke: var(--${colorName});}
+.border-${colorName} {border-color: var(--${colorName});}`
+                );
+                styleTag.appendChild(document.createTextNode(styles.join("\n")));
+                onThemeInjection && onThemeInjection();
+                console.log(styleTag);
+            }
+
+            return () => {
+                const styleTag = getStyleTag("fictoan-custom-colors");
+                styleTag.innerHTML = "";
+            };
+        }, [customColors]);
+
+        const getStyleTag = (id: string): HTMLElement => {
+            let styleTag = document.getElementById(id);
+            if (!styleTag) {
+                styleTag = document.createElement("style");
+                styleTag.id = id;
+                document.head.appendChild(styleTag);
+            }
+            return styleTag;
+        };
+
+        const addCssVariables = (element: HTMLElement, variables: { [key: string]: string }) => {
+            let cssVariables: string | string[] = Object.entries(variables).map(
+                ([variableName, variableValue]) => `--${variableName}: ${variableValue};`
+            );
+            cssVariables = ":root {\n" + cssVariables.join("\n") + "\n}";
+            element.appendChild(document.createTextNode(cssVariables));
+        };
+
         return (
-            <Element<ThemeProviderElementType> as="div" className={currentTheme} ref={ref}>
+            <Element<ThemeProviderElementType> as="div" className={currentTheme} ref={ref} {...props}>
                 {children}
             </Element>
         );
