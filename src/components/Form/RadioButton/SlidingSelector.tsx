5335030451b4e613ffdaffd254a932b3f71fdd38
dhruvmisra <dhruvmisra@live.com>
2024-01-15 15:07:48 +0530

    Fix sliding selector

--- src/components/Form/RadioButton/SlidingSelector.tsx
+++ src/components/Form/RadioButton/SlidingSelector.tsx
@@ -3,24 +3,41 @@ import React from "react";
 import { Element } from "../../Element/Element";
 
 import "./sliding-selector.css";
-import { RadioButtonProps, RadioButtonElementType, RadioGroupProps } from "./constants";
+import { RadioGroupProps } from "./constants";
+import { BaseInputComponent } from "../BaseInputComponent/BaseInputComponent";
 
-export interface SlidingSelectorProps extends RadioButtonProps {
-    options : RadioGroupProps[];
-}
+export interface SlidingSelectorProps extends RadioGroupProps {}
 
-export const SlidingSelector = React.forwardRef(
-    ({ options, onClick, ...props }: RadioGroupProps, ref: React.Ref<RadioButtonElementType>) => {
-        return (
-            <Element<RadioButtonElementType> as="div" data-sliding-selector ref={ref} onClick={onClick}>
+// export const SlidingSelector = React.forwardRef(
+//     ({ options, onClick, ...props }: SlidingSelectorProps, ref: React.Ref<RadioButtonElementType>) => {
+//         return (
+//             <Element<RadioButtonElementType> as="div" data-sliding-selector ref={ref} onClick={onClick}>
+//
+//                 {options.map((option) => (
+//                     <React.Fragment key={option.id}>
+//                         <input type="radio" {...props} {...option} />
+//                         <label htmlFor={option.id}>{option.label}</label>
+//                     </React.Fragment>
+//                 ))}
+//             </Element>
+//         );
+//     },
+// );
+const SlidingSelectorOptions = ({ options, defaultValue, required, ...props }: SlidingSelectorProps) => {
+    return (
+        <Element as="div" data-sliding-selector required={required}>
+            {options.map((option) => (
+                <React.Fragment key={option.id}>
+                    <input type="radio" {...props} {...option} />
+                    <label htmlFor={option.id}>{option.label}</label>
+                </React.Fragment>
+            ))}
+        </Element>
+    );
+};
 
-                {options.map((option) => (
-                    <React.Fragment key={option.id}>
-                        <input type="radio" id={option.id} name={option.id} value={option.value} {...props} />
-                        <label htmlFor={option.id}>{option.label}</label>
-                    </React.Fragment>
-                ))}
-            </Element>
-        );
-    },
-);
+export const SlidingSelector = React.forwardRef((props: SlidingSelectorProps, ref: React.Ref<HTMLDivElement>) => {
+    return (
+        <BaseInputComponent<HTMLDivElement> as={SlidingSelectorOptions} ref={ref} {...props} />
+    );
+});
