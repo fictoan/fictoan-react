938f1f99a7aad9498b2cfe111c3c4cd1ba4b7a01
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2024-11-15 17:15:10 +0530

    Format colour generation file

--- src/scripts/generateColourClasses.js
+++ src/scripts/generateColourClasses.js
@@ -7,28 +7,28 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const colors = {
-    red: { hue: 0, saturation: 84 },
-    salmon: { hue: 16, saturation: 100 },
-    orange: { hue: 30, saturation: 92 },
-    amber: { hue: 40, saturation: 97 },
-    yellow: { hue: 50, saturation: 100 },
-    spring: { hue: 65, saturation: 80 },
-    pistachio: { hue: 78, saturation: 54 },
-    green: { hue: 148, saturation: 50 },
-    teal: { hue: 174, saturation: 62 },
-    cyan: { hue: 176, saturation: 78 },
-    sky: { hue: 194, saturation: 97 },
-    blue: { hue: 212, saturation: 100 },
-    indigo: { hue: 237, saturation: 66 },
-    violet: { hue: 258, saturation: 55 },
-    purple: { hue: 311, saturation: 47 },
-    pink: { hue: 336, saturation: 96 },
-    crimson: { hue: 340, saturation: 64 },
-    brick: { hue: 0, saturation: 68 },
-    sienna: { hue: 15, saturation: 56 },
-    brown: { hue: 24, saturation: 34 },
-    slate: { hue: 209, saturation: 12 },
-    grey: { hue: 0, saturation: 0 },
+    red       : { hue : 0, saturation : 84 },
+    salmon    : { hue : 16, saturation : 100 },
+    orange    : { hue : 30, saturation : 92 },
+    amber     : { hue : 40, saturation : 97 },
+    yellow    : { hue : 50, saturation : 100 },
+    spring    : { hue : 65, saturation : 80 },
+    pistachio : { hue : 78, saturation : 54 },
+    green     : { hue : 148, saturation : 50 },
+    teal      : { hue : 174, saturation : 62 },
+    cyan      : { hue : 176, saturation : 78 },
+    sky       : { hue : 194, saturation : 97 },
+    blue      : { hue : 212, saturation : 100 },
+    indigo    : { hue : 237, saturation : 66 },
+    violet    : { hue : 258, saturation : 55 },
+    purple    : { hue : 311, saturation : 47 },
+    pink      : { hue : 336, saturation : 96 },
+    crimson   : { hue : 340, saturation : 64 },
+    brick     : { hue : 0, saturation : 68 },
+    sienna    : { hue : 15, saturation : 56 },
+    brown     : { hue : 24, saturation : 34 },
+    slate     : { hue : 209, saturation : 12 },
+    grey      : { hue : 0, saturation : 0 },
 };
 
 const levels = 10;
@@ -44,10 +44,18 @@ function generateCSS() {
     });
 
     Object.entries(colors).forEach(([colorName, { hue, saturation }]) => {
-        for (let i = 1; i <= (2 * levels - 1); i++) {
-            const lightness = Math.round((i * 100) / (2 * levels));
+        for (let i = 1; i <= (
+            2 * levels - 1
+        ); i++) {
+            const lightness = Math.round((
+                i * 100
+            ) / (
+                2 * levels
+            ));
             if (i < levels) {
-                const level = (levels - i) * 10;
+                const level = (
+                    levels - i
+                ) * 10;
                 const baseClassName = `${colorName}-dark${level}`;
                 css += `  --${baseClassName}: hsl(${hue}, ${saturation}%, ${lightness}%);\n`;
                 alphaLevels.forEach(alpha => {
@@ -55,7 +63,9 @@ function generateCSS() {
                     css += `  --${baseClassName}-opacity${alpha}: hsla(${hue}, ${saturation}%, ${lightness}%, ${alphaValue});\n`;
                 });
             } else if (i > levels) {
-                const level = (i - levels) * 10;
+                const level = (
+                    i - levels
+                ) * 10;
                 const baseClassName = `${colorName}-light${level}`;
                 css += `  --${baseClassName}: hsl(${hue}, ${saturation}%, ${lightness}%);\n`;
                 alphaLevels.forEach(alpha => {
@@ -74,10 +84,20 @@ function generateCSS() {
     css += "}\n\n";
 
     Object.entries(colors).forEach(([colorName, { hue, saturation }]) => {
-        for (let i = 1; i <= (2 * levels - 1); i++) {
-            const lightness = Math.round((i * 100) / (2 * levels));
-            let baseClassName = i < levels ? `${colorName}-dark${(levels - i) * 10}` :
-                i > levels ? `${colorName}-light${(i - levels) * 10}` : colorName;
+        for (let i = 1; i <= (
+            2 * levels - 1
+        ); i++) {
+            const lightness = Math.round((
+                i * 100
+            ) / (
+                2 * levels
+            ));
+            let baseClassName = i < levels ? `${colorName}-dark${(
+                    levels - i
+                ) * 10}` :
+                i > levels ? `${colorName}-light${(
+                    i - levels
+                ) * 10}` : colorName;
 
             prefixes.forEach(prefix => {
                 css += prefix === "border" ?
@@ -113,7 +133,7 @@ function main() {
     const outputPath = path.resolve(__dirname, "../styles/colours.css");
     const dir = path.dirname(outputPath);
     if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
+        fs.mkdirSync(dir, { recursive : true });
     }
     fs.writeFileSync(outputPath, css);
     console.log("Generated colours.css successfully!");
