cde1b9e5d0230aec88bd88b328c025de12385c53
Sujan Sundareswaran <sujan.sundareswaran@gmail.com>
2024-12-05 15:52:54 +0530

    Fix Range input type issue

--- src/components/Form/Range/Range.tsx
+++ src/components/Form/Range/Range.tsx
@@ -1,13 +1,17 @@
+// FRAMEWORK ===========================================================================================================
 import React from "react";
 
+// FICTOAN =============================================================================================================
 import { Element } from "../../Element/Element";
 import { Div } from "../../Element/Tags";
 import { BaseInputComponent } from "../BaseInputComponent/BaseInputComponent";
 import { InputLabel } from "../InputLabel/InputLabel";
 import { Text } from "../../Typography/Text";
 
+// STYLES ==============================================================================================================
 import "./range.css";
 
+// TYPES ===============================================================================================================
 import { CommonAndHTMLProps } from "../../Element/constants";
 import { InputCommonProps } from "../BaseInputComponent/constants";
 
@@ -16,20 +20,20 @@ export interface RangeCustomProps {
     max      ? : number;
     step     ? : number;
     suffix   ? : string;
-    onChange ? : (value: number) => void;
+    onChange ?: (value: number) => void;
 }
 
 export type RangeElementType = HTMLInputElement;
-export type RangeProps = CommonAndHTMLProps<RangeElementType> & InputCommonProps & RangeCustomProps;
+export type RangeProps = Omit<CommonAndHTMLProps<RangeElementType>, "onChange"> & InputCommonProps & RangeCustomProps;
 
+// COMPONENT ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 export const Range = React.forwardRef(({ label, value, suffix, onChange, ...props }: RangeProps,
      ref: React.Ref<RangeElementType>
     ) => {
-        const handleChange = (val: string) => {
-            if (onChange) {
-                onChange(parseFloat(val));
-            }
-        };
+    const handleChange = (value: string) => {
+        // Convert the string to a number and call our number handler
+        onChange?.(parseFloat(value));
+    };
 
         return (
             <Element<RangeElementType> data-range ref={ref}>
@@ -46,7 +50,7 @@ export const Range = React.forwardRef(({ label, value, suffix, onChange, ...prop
                 <BaseInputComponent
                     as="input"
                     type="range"
-                    value={value}
+                    value={value?.toString()} // Clever, eh?
                     onChange={handleChange}
                     {...props}
                 />
